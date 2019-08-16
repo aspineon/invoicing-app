ec339d0dfdf7fcfae1ac5b0006ee6d496d6e71bd
andrzej musiol <bigeyedes@gmail.com>
2019-08-16 12:40:45 +0200

    Basic form and PDF generator

--- app/main.py
+++ app/main.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+from fpdf import FPDF
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
 	return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+
+	number = request.form['invoice_number']
+	date = request.form['invoice_date']
+	service = request.form['service_delivered']
+
+	data = [number, date, service]
+
+	pdf = FPDF(orientation='P', unit='mm', format='A4')
+	pdf.add_page()
+	pdf.set_font('Arial', size=12)
+
+	for item in data:
+		pdf.cell(0, 10, str(item), 0, 1)
+
+	pdf.output('test.pdf', 'F')
+
+	return render_template('submit.html')
 
 if __name__=='__main__':
 	app.run(debug=True)
