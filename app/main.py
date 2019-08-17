7b3cbde07b9a2061ee10ecd12ad176934dd6616f
andrzej musiol <bigeyedes@gmail.com>
2019-08-17 13:31:26 +0200

    File upload, new dynamic service_inputs and styles

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
+import os
 from flask import Flask, render_template, url_for, request
 from fpdf import FPDF
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = '/files'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -13,8 +19,23 @@ def submit():
 	number = request.form['invoice_number']
 	date = request.form['invoice_date']
 	service = request.form['service_delivered']
+	file = request.files['file']
+	file.save(secure_filename(file.filename))
 
 	data = [number, date, service]
+
+	class PDF(FPDF):
+		def header(self):
+			# Logo
+			self.image(file.filename, 10, 8, 33)
+			# Arial bold 15
+			self.set_font('Arial', 'B', 15)
+			# Move to the right
+			self.cell(80)
+			# Title
+			self.cell(30, 10, 'Faktura: ' + str(number), 1, 0, 'C')
+			# Line break
+			self.ln(20)
 
 	pdf = FPDF(orientation='P', unit='mm', format='A4')
 	pdf.add_page()
@@ -23,7 +44,7 @@ def submit():
 	for item in data:
 		pdf.cell(0, 10, str(item), 0, 1)
 
-	pdf.output('test.pdf', 'F')
+	pdf.output(str(number) + '.pdf', 'F')
 
 	return render_template('submit.html')
 
